310f8bbc7c2ebc758f72b874322f3e843717f1d8
Temporary-summer <133712574+Temporary-summer@users.noreply.github.com>
2023-07-24 11:14:06 -0400

    Testing Donar page view

--- food/test.py
+++ food/test.py
@@ -6,3 +6,20 @@ from .models import DonarRegister_Model, Complaint_Model, Suggestion_Model, Rati
 
 # Importing reverse function to generate URLs for testing
 from django.urls import reverse
+from django.test import TestCase
+from django.urls import reverse
+from myapp.models import DonarRegister_Model
+
+class DonorPageViewTest(TestCase):
+
+    def setUp(self):
+        # Preparing test data: Creating a DonarRegister_Model object
+        DonarRegister_Model.objects.create(name='vinay', password='vinay123', email='vinay@gmail.com', phone='9999988888', address='vijayawada')
+
+    def test_view_url_exists_at_proper_location(self):
+        # Contract: Testing if the view for the specified URL exists and returns a 200 status code
+        url = reverse('home')  # Assuming the URL name for the homepage is 'home'
+        resp = self.client.get(url)
+
+        # Assertion: Ensuring the view for the specified URL exists and returns a 200 status code
+        self.assertEqual(resp.status_code, 200)
