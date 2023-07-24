430f8d6c029aa2ab6e869fab55b8dbf20a525797
Temporary-summer <133712574+Temporary-summer@users.noreply.github.com>
2023-07-24 11:18:06 -0400

    Testing User rating

--- food/test.py
+++ food/test.py
@@ -6,3 +6,27 @@ from .models import DonarRegister_Model, Complaint_Model, Suggestion_Model, Rati
 
 # Importing reverse function to generate URLs for testing
 from django.urls import reverse
+
+
+from django.test import TestCase
+from myapp.models import Rating_Model
+
+class Rating_ModelTest(TestCase):
+
+    def setUp(self):
+        # Preparing test data: Creating a Rating_Model object
+        Rating_Model.objects.create(name='vinay', rating='5')
+
+    def test_text_content(self):
+        # Retrieving the Rating_Model object created in setUp
+        ratingregister = Rating_Model.objects.get(id=1)
+
+        # Contract: Testing the name and rating attributes of the Rating_Model object
+        # Expected name and rating values are defined based on the setUp data
+        rate_name = f'{ratingregister.name}'
+        rate_rating = f'{ratingregister.rating}'
+
+        # Assertion: Ensuring the name and rating values match the expected values
+        self.assertEqual(rate_name, 'vinay')
+        self.assertEqual(rate_rating, '5')
+
