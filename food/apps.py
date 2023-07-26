6374bb8e7eaeba50a3c949230959531580778019
SuyesSapkota <57615996+SuyesSapkota@users.noreply.github.com>
2023-07-26 15:44:27 -0400

    updating apps.py using the Programme By Contract and Literate Programming

--- food/apps.py
+++ food/apps.py
@@ -1,5 +1,15 @@
+"""
+Code facilitates the customization of settings and behavior for the 'food' app by 
+defining a custom AppConfig class. It helps keep the app-specific configurations 
+separate from the project-wide settings, promoting modularity and maintainability 
+in Django projects.
+"""
+
+# Importing the AppConfig class from the django.apps module.
 from django.apps import AppConfig
 
-
+# Defining a new class named 'FoodConfig' that inherits from AppConfig.
 class FoodConfig(AppConfig):
+
+    # Setting the 'name' attribute of the class to 'food'.
     name = 'food'
