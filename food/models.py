34981516ce5792dc201f055abb87488eeac732ff
SuyesSapkota <57615996+SuyesSapkota@users.noreply.github.com>
2023-07-26 16:32:44 -0400

    Enhancing the models.py file using the principles of Program by Contract and Literate Programming

--- food/models.py
+++ food/models.py
@@ -1,138 +1,181 @@
+# Imports the models module from Django, which provides tools for defining and interacting with database models.
 from django.db import models
 
-# Create your models here.
-
-
+# Donar_Model represents donors' information
 class Donar_Model(models.Model):
-    fullname = models.CharField(max_length=40)  # Represents the full name of the donor (maximum length: 40 characters).
-    email = models.CharField(max_length=40)  # Represents the email of the donor (maximum length: 40 characters).
-    fooditems = models.CharField(max_length=100)  # Represents the donated food items (maximum length: 100 characters).
-    address = models.TextField(default="enter the address")  # Represents the address of the donor (default value: "enter the address").
-    phone = models.CharField(max_length=20)  # Represents the phone number of the donor (maximum length: 20 characters).
-    orphanage = models.CharField(max_length=30)  # Represents the orphanage to which the donation is made (maximum length: 30 characters).
-    date = models.DateField()  # Represents the date of the donation.
+    """
+    Donar_Model to store donor information related to food donations for orphanages. 
+    The model is used in a web application to manage and interact with donor data, allowing 
+    the application to handle donor records efficiently.
+    Attributes:
+        fullname (str): Full name of the donor.
+        email (str): Email address of the donor.
+        fooditems (str): Food items donated by the donor.
+        foodpicture (ImageField): Image of the donated food items (optional).
+        address (str): Address of the donor.
+        phone (str): Phone number of the donor.
+        orphanage (str): Name of the orphanage where the donation is made.
+        date (date): Date of the donation.
+    """
+    # Full name field with a maximum length of 40 characters
+    fullname=models.CharField(max_length=40)
+    # Email field with a maximum length of 40 characters
+    email=models.CharField(max_length=40)
+    # Food items field with a maximum length of 100 characters
+    fooditems=models.CharField(max_length=100)
+    # ImageField to store the image of the donated food items (optional)
+    foodpicture = models.ImageField(default="")
+    # Address field with a default value "enter the address" for new donors
+    address=models.TextField(default="enter the address")
+    # Phone field with a maximum length of 20 characters
+    phone=models.CharField(max_length=20)
+    # Orphanage field with a maximum length of 30 characters
+    orphanage=models.CharField(max_length=30)
+    # Date field to store the date of the donation
+    date=models.DateField()
     
     def __str__(self):
-        """
-        Returns a string representation of the Donar_Model object.
-        """
+    # This method is used to represent the object as a string.
+    # It is intended to provide a human-readable string representation of the object.
         return self.fullname
 
 
-
+# DonarRegister_Model represents registered donors
 class DonarRegister_Model(models.Model):
     """
-    Represents a donor registration.
+    DonarRegister_Model to store information about registered donors. 
+    The model is used in a web application to manage and interact with 
+    donor registration data, allowing the application to handle user accounts 
+    and profiles efficiently.
 
-    Fields:
-    - name: CharField with a maximum length of 40 characters, representing the user's name.
-    - password: CharField with a maximum length of 40 characters, representing the user's password.
-    - email: CharField with a maximum length of 50 characters, representing the user's email.
-    - phone: CharField with a maximum length of 20 characters, representing the user's phone number.
-    - address: TextField with a default value of "Enter the address", representing the user's address.
-
-    Contracts:
-    - Precondition: The length of the name field is less than or equal to 40 characters.
-    - Precondition: The length of the password field is less than or equal to 40 characters.
-    - Precondition: The length of the email field is less than or equal to 50 characters.
-    - Precondition: The length of the phone field is less than or equal to 20 characters.
-    - Postcondition: The __str__() method returns the user's name as a string representation of the DonarRegister_Model object.
+    Attributes:
+        name (str): User name of the registered donor.
+        password (str): Password for the donor account.
+        email (str): Email address of the registered donor.
+        phone (str): Phone number of the registered donor.
+        address (str): Address of the registered donor.
     """
-
-    name = models.CharField(max_length=40)
-    password = models.CharField(max_length=40)
-    email = models.CharField(max_length=50)
-    phone = models.CharField(max_length=20)
-    address = models.TextField(default="Enter the address")
+    # User name field with a maximum length of 40 characters
+    name=models.CharField(max_length=40)
+    # Password field with a maximum length of 40 characters
+    password=models.CharField(max_length=40)
+    # Email field with a maximum length of 50 characters
+    email=models.CharField(max_length=50)
+    # Phone field with a maximum length of 20 characters
+    phone=models.CharField(max_length=20)
+    # Address field with a default value "Enter the address" for new donors
+    address=models.TextField(default="Enter the address")
 
     def __str__(self):
-        """
-        Returns a string representation of the DonarRegister_Model object.
-        """
+        # This method returns the string representation of the object based on its 'name' attribute.
         return self.name
 
 
 class Agent_Model(models.Model):
-    name = models.CharField(max_length=40)  # Represents the name of the agent (maximum length: 40 characters).
-    password = models.CharField(max_length=40)  # Represents the password of the agent (maximum length: 40 characters).
-    email = models.CharField(max_length=50)  # Represents the email of the agent (maximum length: 50 characters).
-    phone = models.CharField(max_length=20)  # Represents the phone number of the agent (maximum length: 20 characters).
-    address = models.TextField(default="Enter the address")  # Represents the address of the agent (default value: "Enter the address").
+    """
+    Code implements a Django model named Agent_Model to store information about agents. 
+    The model is used in a web application to manage and interact with agent data, allowing 
+    the application to handle agent accounts and profiles efficiently.
+    Attributes:
+        name (str): User name of the agent.
+        password (str): Password for the agent account.
+        email (str): Email address of the agent.
+        phone (str): Phone number of the agent.
+        address (str): Address of the agent.
+    """
+    # User name field with a maximum length of 40 characters
+    name=models.CharField(max_length=40)
+    # Password field with a maximum length of 40 characters
+    password=models.CharField(max_length=40)
+    # Email field with a maximum length of 50 characters
+    email=models.CharField(max_length=50)
+    # Phone field with a maximum length of 20 characters
+    phone=models.CharField(max_length=20)
+    # Address field with a default value "Enter the address" for new agents
+    address=models.TextField(default="Enter the address")
 
     def __str__(self):
-        """
-        Returns a string representation of the Agent_Model object.
-        """
+        # This method returns the string representation of the object based on its 'name' attribute.
         return self.name
 
 
-
+# Assign_Model represents the assignment of agents to customers
 class Assign_Model(models.Model):
-    cid = models.IntegerField(primary_key=True)  # Represents the primary key field for the assignment model (integer type).
-    aid = models.IntegerField()  # Represents the agent ID for the assignment (integer type).
+    """
+    Django model named Assign_Model to represent the assignment of agents to customers. 
+    The model is used in a web application to manage and interact with agent-customer 
+    assignment data, ensuring that the correct agents are associated with their respective customers.
+     Attributes:
+        cid (int): Customer ID.
+        aid (int): Agent ID.
+    """
+    # Customer ID field with primary key constraint
+    cid=models.IntegerField(primary_key=True)
+    # Agent ID field
+    aid=models.IntegerField()
 
     def __str__(self):
-        """
-        Returns a string representation of the Assign_Model object.
-        """
-        return str(self.cid)
-             return self.cid
+    # This method returns the string representation of the object, which is the 'cid' attribute converted to a string.
+        return self.cid
+    
+
+# Complaint_Model represents customer complaints
+class Complaint_Model(models.Model):
+    """
+    code implements a Django model named Complaint_Model to represent and store customer complaints. 
+    The model is used in a web application to manage and interact with complaint data, enabling the application to handle and resolve customer concerns efficiently.
+    Attributes:
+        name (str): Name of the customer making the complaint.
+        message (str): Complaint message from the customer.
+    """
+    # Name field with a maximum length of 30 characters and a default value ""
+    name = models.CharField(max_length=30,default="")
+    # Complaint message field with a default value "Enter the complaint Message"
+
+    message = models.TextField(default = "Enter the complaint Message")
+
+    def __str__(self):
+        # This method returns the string representation of the object based on its 'name' attribute.
+        return self.name
+
+    
+
+class Suggestion_Model(models.Model):
+    """
+    This code implements the Suggestion_Model Django model, which is used to describe and store user recommendations.
+      A web application uses the model to maintain and interact with recommendation data so that it may hear from users 
+      and make changes in response to their ideas.
+
+    Attributes:
+        name (str): Name of the customer making the suggestion.
+        message (str): Suggestion message from the customer.
+    """
+
+    # Name field with a maximum length of 30 characters
+    name = models.CharField(max_length = 30)
+    # Suggestion message field with a default value "Enter the suggestion Message"
+    message = models.TextField(default="Enter the suggestion Message")
+
+    def __str__(self):
+    # This method returns the string representation of the object based on its 'name' attribute.
+        return self.name
 
 
-
-
+# Rating_Model represents customer ratings
 class Rating_Model(models.Model):
     """
-    This class represents the Rating Model for storing ratings in the database.
-
-    Contract:
-    - Invariants: Each instance of this model will have a 'name' and a 'rating' field.
-
-    Fields:
-    - name (CharField): The name associated with the rating (maximum length: 30 characters).
-    - rating (CharField): The rating value itself (maximum length: 30 characters).
-
-    Methods:
-    - __str__: Returns the name as a string representation of the model instance.
+    code implements a Django model named Rating_Model to represent and store customer ratings. 
+    The model is used in a web application to manage and interact with rating data, providing insights 
+    into customer satisfaction and feedback for various services or products.
+    Attributes:
+        name (str): Name of the customer giving the rating.
+        rating (str): Customer's rating value (e.g., 1, 2, 3, 4, 5).
     """
-
-    name = models.CharField(max_length=30)
+    # Name field with a maximum length of 30 characters
+    name = models.CharField(max_length = 30)
+    # Rating field with a maximum length of 30 characters
     rating = models.CharField(max_length=30)
 
     def __str__(self):
-        """
-        Get a string representation of the model instance.
-
-        Returns:
-        - str: The 'name' field value as a string representation of the instance.
-        """
-        return self.name
-
-# Defining the Complaint_Model class which represents a model for complaints
-class Complaint_Model(models.Model):
-
-    # Defining a field "name" to store the name of the complainant
-    name = models.CharField(max_length=30, default="")
-
-    # Defining a field "message" to store the complaint message
-    message = models.TextField(default="Enter the complaint Message")
-
-    # Defining the __str__ method to represent the model instance as a string (name in this case)
-    def __str__(self):
-        return self.name
-
-# Defining the Suggestion_Model class which represents a model for suggestions
-class Suggestion_Model(models.Model):
-
-    # Defining a field "name" to store the name of the person providing the suggestion
-    name = models.CharField(max_length=30)
-
-    # Defining a field "message" to store the suggestion message
-    message = models.TextField(default="Enter the suggestion Message")
-
-    # Defining the __str__ method to represent the model instance as a string (name in this case)
-    def __str__(self):
-        return self.name
-
-
-
+    # This method returns the string representation of the object based on its 'name' attribute.
+     return self.name
