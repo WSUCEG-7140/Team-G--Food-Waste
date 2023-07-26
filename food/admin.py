4c127a844ae43dd9595d10853e6a2b7174bab35a
SuyesSapkota <57615996+SuyesSapkota@users.noreply.github.com>
2023-07-26 15:43:21 -0400

    Updating admin.py as per Programme By Contract and Literate Programming

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,11 @@
+"""
+The 'admin' module provides a powerful web-based interface for managing your Django application's data. 
+It allows you to create, read, update, and delete records in your database, as well as manage users and 
+permissions. The admin site is automatically generated based on your models and registered with the admin.
+site, making it quick and easy to get a basic CRUD (Create, Read, Update, Delete) interface up and running 
+for your app. Remember to customize the admin interface to fit your application's specific needs and
+ make it user-friendly.
+"""
 from django.contrib import admin
 
 # Register your models here.
